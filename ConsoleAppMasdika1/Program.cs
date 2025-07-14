db8b4a3074ef8f3a59b3d9beb881fae35aa2f454
Masdikaa <masdikailhanmansiz@gmail.com>
2025-07-14 10:01:40 +0700

    gitignore

--- ConsoleAppMasdika1/Program.cs
+++ ConsoleAppMasdika1/Program.cs
@@ -6,7 +6,7 @@ namespace NamaProyek
    {
        static void Main(string[] args)
        {
-          Console.WriteLine("Hello, World!");
+          Console.WriteLine("Masdika Ilhan Mansiz !");
        }
    }
 }
--- ConsoleAppMasdika1/obj/Debug/net8.0/ConsoleAppMasdika1.AssemblyInfo.cs
+++ ConsoleAppMasdika1/obj/Debug/net8.0/ConsoleAppMasdika1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleAppMasdika1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a444ed4daf4556bebdb54d349edd96f1d8943ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleAppMasdika1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleAppMasdika1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
