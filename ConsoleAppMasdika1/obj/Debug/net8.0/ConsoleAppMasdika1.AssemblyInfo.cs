b829ca388baa6f7f5d97305440fd8772b1e7e72b
Masdikaa <masdikailhanmansiz@gmail.com>
2025-07-14 21:24:51 +0700

    editorconfig

--- ConsoleAppMasdika1/obj/Debug/net8.0/ConsoleAppMasdika1.AssemblyInfo.cs
+++ ConsoleAppMasdika1/obj/Debug/net8.0/ConsoleAppMasdika1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleAppMasdika1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db8b4a3074ef8f3a59b3d9beb881fae35aa2f454")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e92051f9c7046b9f985acb0e90015be23e15b02")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleAppMasdika1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleAppMasdika1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
